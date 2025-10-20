d45755b91e54f4b16919fd08c5661075e47c7778
Alexander-2049 <a.alexander.2049@gmail.com>
2025-10-21 01:18:26 +0300

    Enhance header and rooms components with improved styling and form functionality

--- frontend/src/app/rooms/rooms.component.ts
+++ frontend/src/app/rooms/rooms.component.ts
@@ -1,11 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss',
 })
-export class RoomsComponent {}
+export class RoomsComponent {
+  headForm = new FormGroup({
+    searchCreateInputBar: new FormControl('', Validators.required),
+  });
+}
