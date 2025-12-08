0e23979729ee709f89a9c01e8c7bb4be4318c6a0
Alexander2049 <a.alexander.2049@gmail.com>
2025-12-08 12:46:58 +0200

    Implement profile guard and enhance profile component with form for username input

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,15 +2,18 @@ import { Routes } from '@angular/router';
 import { RoomsComponent } from './rooms/rooms.component';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
 import { ProfileComponent } from './profile/profile.component';
+import { profileGuard } from './profile.guard';
 
 export const routes: Routes = [
   {
     path: 'rooms',
     component: RoomsComponent,
+    canActivate: [profileGuard],
   },
   {
     path: 'rooms/:id',
     component: ChatWindowComponent,
+    canActivate: [profileGuard],
   },
   {
     path: 'profile',
--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { randomFloatNeg1_5To1_5 } from '../utils/random';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { StorageService } from '../storage.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  constructor(private storage: StorageService) {}
+
+  profileSettingsForm = new FormGroup({
+    username: new FormControl(
+      this.storage.get('username') || '',
+      Validators.required
+    ),
+  });
   wrapperDegAngle = randomFloatNeg1_5To1_5();
+
+  saveProfileSettings() {
+    const username = this.profileSettingsForm.get('username');
+    if (username === null || username.value === null) return;
+
+    this.storage.set('username', username.value);
+    // if (message.hasError('required') || message.value.trim() === '') {
+    //   this.triggerShake();
+    //   return;
+    // }
+  }
 }
